c46ce2f857ccef21bace5ba44b5d0dad23bbc5d5
Zai <allenzaire@gmail.com>
2025-05-02 20:52:38 -0400

    started my new project for state management

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header (){
+    return (
+        <header>
+            <nav className="nav content">
+                {/* <img src="./images/chef-claude-icon.png" /> */}
+                <h1>Chef Claude</h1>
+            </nav>
+        </header>
+    )
+}
